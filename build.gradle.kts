ff0fee75747459b9bdfdb4211f7c2da145631b3c
Scott Lamb <sentientscythe@gmail.com>
2026-07-04 02:34:27 -0600

    temp

---
+++ build.gradle.kts
@@ -0,0 +1,189 @@
+import java.util.*
+
+plugins {
+    id("dev.architectury.loom")
+    id("architectury-plugin")
+    id("me.modmuss50.mod-publish-plugin")
+    id("com.gradleup.shadow")
+}
+
+val minecraft = stonecutter.current.version
+val loader = loom.platform.get().name.lowercase()
+
+version = "${mod.version}+$minecraft"
+group = mod.group
+base {
+    archivesName.set("${mod.id}-$loader")
+}
+
+architectury.common(stonecutter.tree.branches.mapNotNull {
+    if (stonecutter.current.project !in it) null
+    else it.prop("loom.platform")
+})
+repositories {
+    maven("https://maven.minecraftforge.net/")
+    maven("https://maven.neoforged.net/releases/")
+
+    //modmenu
+    maven("https://maven.terraformersmc.com/")
+    //placeholder api (modmenu depencency)
+    maven("https://maven.nucleoid.xyz/")
+}
+dependencies {
+    minecraft("com.mojang:minecraft:$minecraft")
+    
+    if (stonecutter.eval(minecraft, ">=1.14.4")) {
+        mappings(loom.officialMojangMappings())
+    } else {
+        // Placeholder for MCP mappings for older versions
+    }
+    
+    testImplementation("junit:junit:4.13.2")
+    
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.10.0")
+
+    if (loader == "forge") {
+        val forgeVer = project.findProperty("forge_version")?.toString() ?: project.findProperty("minecraft_version")?.toString() ?: "14.23.5.2860"
+        "forge"("net.minecraftforge:forge:${minecraft}-${forgeVer}")
+    }
+    if (loader == "neoforge") {
+        val neoVer = project.findProperty("neo_version")?.toString() ?: project.findProperty("forge_version")?.toString() ?: "21.1.148"
+        "neoForge"("net.neoforged:neoforge:${neoVer}")
+    }
+}
+
+loom {
+    decompilers {
+        get("vineflower").apply { // Adds names to lambdas - useful for mixins
+            options.put("mark-corresponding-synthetics", "1")
+        }
+    }
+    if (loader == "forge") {
+        forge.mixinConfigs(
+            "template-common.mixins.json",
+            "template-forge.mixins.json",
+        )
+    }
+}
+
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.inputStream())
+}
+publishMods {
+    val modrinthToken = localProperties.getProperty("publish.modrinthToken", "")
+    val curseforgeToken = localProperties.getProperty("publish.curseforgeToken", "")
+
+
+    file = project.tasks.remapJar.get().archiveFile
+    dryRun = modrinthToken == null || curseforgeToken == null
+
+    displayName = "${mod.name} ${loader.replaceFirstChar { it.uppercase() }} ${property("mod.mc_title")}-${mod.version}"
+    version = mod.version
+    changelog = rootProject.file("CHANGELOG.md").readText()
+    type = BETA
+
+    modLoaders.add(loader)
+
+    val targets = property("mod.mc_targets").toString().split(' ')
+    modrinth {
+        projectId = property("publish.modrinth").toString()
+        accessToken = modrinthToken
+        targets.forEach(minecraftVersions::add)
+        if (loader == "fabric") {
+            requires("fabric-api")
+            optional("modmenu")
+        }
+    }
+
+    curseforge {
+        projectId = property("publish.curseforge").toString()
+        accessToken = curseforgeToken.toString()
+        targets.forEach(minecraftVersions::add)
+        if (loader == "fabric") {
+            requires("fabric-api")
+            optional("modmenu")
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+    val java = if (stonecutter.eval(minecraft, ">=1.20.5")) JavaVersion.VERSION_21 else JavaVersion.VERSION_17
+    targetCompatibility = java
+    sourceCompatibility = java
+}
+
+val shadowBundle: Configuration by configurations.creating {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+
+tasks.shadowJar {
+    configurations = listOf(shadowBundle)
+    archiveClassifier = "dev-shadow"
+}
+
+tasks.remapJar {
+    inputFile.set(tasks.shadowJar.flatMap { it.archiveFile })
+    archiveClassifier = null
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.jar {
+    archiveClassifier = "dev"
+}
+
+val buildAndCollect = tasks.register<Copy>("buildAndCollect") {
+    group = "versioned"
+    description = "Must run through 'chiseledBuild'"
+    from(tasks.remapJar.get().archiveFile, tasks.remapSourcesJar.get().archiveFile)
+    into(rootProject.layout.buildDirectory.file("libs/${mod.version}/$loader"))
+    dependsOn("build")
+}
+
+if (stonecutter.current.isActive) {
+    rootProject.tasks.register("buildActive") {
+        group = "project"
+        dependsOn(buildAndCollect)
+    }
+
+    rootProject.tasks.register("runActive") {
+        group = "project"
+        dependsOn(tasks.named("runClient"))
+    }
+}
+
+tasks.processResources {
+    properties(
+        listOf("fabric.mod.json"),
+        "id" to mod.id,
+        "name" to mod.name,
+        "version" to mod.version,
+        "minecraft" to mod.prop("mc_dep_fabric")
+    )
+    properties(
+        listOf("META-INF/mods.toml", "pack.mcmeta"),
+        "id" to mod.id,
+        "name" to mod.name,
+        "version" to mod.version,
+        "minecraft" to mod.prop("mc_dep_forgelike")
+    )
+    properties(
+        listOf("META-INF/neoforge.mods.toml", "pack.mcmeta"),
+        "id" to mod.id,
+        "name" to mod.name,
+        "version" to mod.version,
+        "minecraft" to mod.prop("mc_dep_forgelike")
+    )
+}
+
+tasks.build {
+    group = "versioned"
+    description = "Must run through 'chiseledBuild'"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.minecraftforge.net")
+        maven("https://maven.neoforged.net/releases/")
+        maven("https://maven.kikugie.dev/snapshots")
+    }
+}
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.5"
+}
+
+stonecutter {
+    centralScript = "build.gradle.kts"
+    kotlinController = true
+    shared {
+        fun mc(loader: String, vararg versions: String) {
+            for (version in versions) vers("$version-$loader", version)
+        }
+        mc("forge", "1.20.1")
+        mc("neoforge", "1.20.4", "1.21.1")
+    }
+    create(rootProject)
+}
+
+rootProject.name = "ProjectE"
